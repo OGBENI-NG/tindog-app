1a9a156b5d6e6673bc5bc6ce9c57ccb76ee33da5
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2022-10-14 17:36:54 -0600

    I add profile page
    I add profile page and registration form

--- Utility.js
+++ Utility.js
@@ -15,6 +15,55 @@ function likeAvatar() {
     </div>`
 }
 
+
+function formContainer() {
+    return`
+    <form action="#" id="tindog-form">
+        <div class="close-modal-btn-container">
+            <button class="close-btn" id="close-btn"><</button>
+        </div>		
+        <h2 class="form-text">Register here</h2>
+        <input type="file">
+        <input type="text" name="fullName"
+            aria-label="fullName"
+            placeholder="Enter your fullname"
+            required
+        >
+        <input type="email" name="email"
+            aria-label="email"
+            placeholder="Enter your email"
+            required
+        >
+        <input type="password" name="password"
+            aria-label="password"
+            placeholder="Enter your password"
+            required
+        >
+        <input type="text" name="bio"
+            aria-label="biograpy"
+            placeholder="Bio"
+            required
+        >
+        <button type="submit" class="sunmit-btn">Submit</button>
+    </form>`
+}
+
+function loading() {
+    return `
+    <div class="loading">
+        <svg version="1.2" height="300" width="600" xmlns="http://www.w3.org/2000/svg"
+            ="0 0 60 60" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <path id="pulsar" stroke="rgba(0,155,155,1)" 
+            fill="none" stroke-width="1"stroke-linejoin="round"
+            d="M0,90L250,90Q257,60 262,87T267,95 270,88 273,92t6,35
+            7,-60T290,127 297,107s2,-11 10,-10 1,1 8,-10T319,95c6,4
+            8,-6 10,-17s2,10 9,11h210" /> 
+        </svg>
+        <h3 class="loading-txt">Setting up your profile</h3>
+    </div>`
+}
+
+
 function nopeAvatar() {
     return`
     <div class="like-avatar">
@@ -50,4 +99,4 @@ function lastPage() {
 
 
 
-export {btn, likeAvatar, nopeAvatar, welcomePage, lastPage}
+export {btn, likeAvatar, nopeAvatar, welcomePage, lastPage, formContainer, loading}
--- index.js
+++ index.js
@@ -1,33 +1,106 @@
 // Remember to import the data and Dog class!
 import dogsData from "./data.js";
 import DogCharacter from "./Dog.js";
-import {likeAvatar, nopeAvatar, welcomePage, lastPage} from "./Utility.js" 
+import {likeAvatar, nopeAvatar, welcomePage, lastPage, formContainer, loading} from "./Utility.js" 
 
 let dogIndex = ['Bella', 'Teddy', 'Ojugbako', 'Dudu']
 const mainHtml = document.getElementById("dogs-profile")
+const profile = document.getElementById("profile")
+
 
 let isWaiting = false
 
 function welcomeIntro() {
     mainHtml.innerHTML = welcomePage()
+
 }
 welcomeIntro()
 
-function backBtn() {
-   welcomeIntro()
+profile.addEventListener("click", () => {
+    mainHtml.innerHTML = formContainer()
+    closeBtn()
+    submit()
+    
 
+})
+
+
+function closeBtn() {
+    const tinDogForm = document.getElementById("tindog-form")
+    document.getElementById("close-btn").addEventListener("click", () => {
+        tinDogForm.style.display = "none"
+        if(welcomeIntro()) {
+            isWaiting = true
+        } else {
+            joinUs()
+        
+        }
+    })
+   
 }
+
+function submit() {
+    const formDetails = document.getElementById("tindog-form")
+    formDetails.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        const userDetails = new FormData(formDetails)
+        const fullName = userDetails.get("fullName")
+        const email = userDetails.get("email")
+        const bio = userDetails.get("bio")
+        
+        setTimeout(() => {
+            formDetails.innerHTML = loading()
+        }, 2000)  
+            
+        setTimeout(() => {
+            formDetails.innerHTML =`
+                <div class="profile-container">
+                    <img class="profile-avatar" src="images/profile-pics.jpg">
+                </div>
+                <div class="details-container">
+                    <h3 class="profile-details"><span class="profile-text">Fullname:</span> ${fullName}</h3>
+                    <h3 class="profile-details"><span class="profile-text">Bio:</span> ${bio}</h3>
+                    <p class="profile-details"><span class="profile-text">Email:</span> ${email}</p>
+                </div>
+                <button id="done-btn" class="done-btn">Done</button>`
+        },4000)
+
+        setTimeout(() => {
+            doneBtn()
+        }, 5000)
+    })
+    
+}
+
+function doneBtn() {
+    const done = document.getElementById("tindog-form")
+    document.getElementById("done-btn").addEventListener("click", () => {
+         done.style.display = "none"
+         if(welcomeIntro()) {
+             isWaiting = true
+         } else {
+            submit()
+            joinUs()
+         }
+    })
+}
+
+
 
 function joinUs() {
     document.getElementById("join-us-btn").addEventListener("click", getNextDogHtml)
+    
 }
 joinUs()
 
 function getNextDogHtml() {
-    const dogs = new DogCharacter(dogsData.shift())
-    mainHtml.innerHTML = dogs.dogCharacterHtml()
-    render() 
-}
+    setTimeout(() => {
+        const dogs = new DogCharacter(dogsData.shift())
+        mainHtml.innerHTML = dogs.dogCharacterHtml()
+        render()
+    }, 1000)
+}   
 
 function likeBtn() {
     mainHtml.innerHTML += likeAvatar()
@@ -45,12 +118,9 @@ function nextDogProfile() {
     } else if(dogIndex.pop()) {
         setTimeout(() => {
             getNextDogHtml()
-        },1500)
-    } else {
-        setTimeout( () => {
-            mainHtml.innerHTML = lastPage()
-            backHome()
-        }, 1000)  
+        },1200)
+    } else {  
+        backBtn()
     }
     
 }
@@ -63,21 +133,19 @@ function render() {
     
 }
 
-function backHome() {
-    document.getElementById('back-home').addEventListener("click", backBtn)
-    
+
+
+function backBtn() {
+    mainHtml.innerHTML = lastPage() 
+    const mainBack = document.getElementById("dogs-profile")
+    document.getElementById("back-home").addEventListener("click", () => {
+        mainBack.style.display = "none"
+        if(welcomeIntro() ) {
+            isWaiting = true
+        } else {
+            joinUs()
+        }
+    })
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
